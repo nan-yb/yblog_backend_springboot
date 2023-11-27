f084d6d3928a1ecaf2d39fa7e59f4769780f82b0
albin <ybkim@aipim.io>
2023-11-27 17:30:51 +0900

    init project

---
+++ src/main/java/com/yblog/YblogApplication.java
@@ -0,0 +1,15 @@
+package com.yblog;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@SpringBootApplication
+@EnableRedisRepositories
+public class YblogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YblogApplication.class);
+    }
+
+}
