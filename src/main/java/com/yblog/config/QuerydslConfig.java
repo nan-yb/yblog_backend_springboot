475121d5c1cb0ea65fdb55022f3608ebcd5a22b9
yongbin <dydqls5757@gmail.com>
2023-11-28 21:43:22 +0900

    feat : Jpa Config

---
+++ src/main/java/com/yblog/config/QuerydslConfig.java
@@ -0,0 +1,16 @@
+package com.yblog.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class QuerydslConfig {
+
+//    @Bean
+//    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+//        return new JPAQueryFactory(entityManager);
+//    }
+}
