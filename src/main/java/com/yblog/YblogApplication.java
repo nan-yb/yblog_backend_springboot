39cebbaa1a95423e1376e0e0e4db2f0205839b45
yb <dydqls5757@gmail.com>
2023-12-04 15:16:35 +0900

    feat : Init data

--- src/main/java/com/yblog/YblogApplication.java
+++ src/main/java/com/yblog/YblogApplication.java
@@ -1,10 +1,15 @@
 package com.yblog;
 
 
+import com.yblog.domain.common.repository.MemberRepository;
+import com.yblog.entity.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableRedisRepositories
@@ -13,12 +18,8 @@ public class YblogApplication {
 //    @Autowired
 //    MemberRepository memberRepository;
 
-    public static void main(String[] args) {
-        SpringApplication.run(YblogApplication.class);
-    }
-
-    @Bean
-    public void initData(){
+//    @Bean
+//    public void initData(){
 //        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 //        Member member = new Member();
 //        member.setName("test1");
@@ -26,5 +27,10 @@ public class YblogApplication {
 //        member.setEmail("test1");
 //        member.setAuthCd("N");
 //        memberRepository.save(member);
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(YblogApplication.class);
     }
+
 }
