df38b61505373f35159292731936d25b8539dbc3
yongbin_home <dydqls5757@gmail.com>
2023-12-07 22:29:14 +0900

    feat : login

--- src/main/java/com/yblog/domain/main/controller/MainController.java
+++ src/main/java/com/yblog/domain/main/controller/MainController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @RequestMapping(value = "/" , method = RequestMethod.GET)
     public String mainTest(){
-        return "main";
+
+        return "main"
+                ;
     }
 }
