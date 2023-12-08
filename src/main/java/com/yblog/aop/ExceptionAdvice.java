a95142bfa676016b3d45a315d17d458a4e6b8b7e
yb <dydqls5757@gmail.com>
2023-12-08 16:10:10 +0900

    feat : add CustomException

--- src/main/java/com/yblog/aop/ExceptionAdvice.java
+++ src/main/java/com/yblog/aop/ExceptionAdvice.java
@@ -1,9 +1,13 @@
 package com.yblog.aop;
 
 import com.yblog.base.ErrorEntity;
+import com.yblog.exception.CustomException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,4 +27,24 @@ public class ExceptionAdvice {
         log.error("Login Exception({}) - {}", e.getClass().getSimpleName(), e.getMessage());
         return new ErrorEntity(e.getMessage());
     }
+
+    @ExceptionHandler(CustomException.class)
+    protected ErrorEntity handleCustomException(CustomException e) {
+        log.error("CustomException", e);
+        return new ErrorEntity(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ErrorEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+//        log.error("handleMethodArgumentNotValidException", e);
+//        BindingResult bindingResult = e.getBindingResult();
+//        StringBuilder stringBuilder = new StringBuilder();
+//        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+//            stringBuilder.append(fieldError.getField()).append(":");
+//            stringBuilder.append(fieldError.getDefaultMessage());
+//            stringBuilder.append(", ");
+//        }
+        return new ErrorEntity(e.getMessage());
+    }
+
 }
