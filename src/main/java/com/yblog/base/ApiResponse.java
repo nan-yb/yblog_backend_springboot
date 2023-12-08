d0c041758bd81aeb47b408c82c745f864dd567f5
albin <ybkim@aipim.io>
2023-12-08 15:46:57 +0900

    feat : 공통 response

--- src/main/java/com/yblog/base/ApiResponse.java
+++ src/main/java/com/yblog/base/ApiResponse.java
@@ -1,21 +1,34 @@
 package com.yblog.base;
 
+import com.yblog.exception.CustomException;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class ApiResponse {
 
-    private Object result;
-
-    private int resultCode;
-
-    private String resultMsg;
+    private Object data;
+    private HttpStatus status;
+    private String statusText;
+    private CustomException exception;
 
     @Builder
-    public ApiResponse(final Object result, final int resultCode, final String resultMsg) {
-        this.result = result;
-        this.resultCode = resultCode;
-        this.resultMsg = resultMsg;
+    public ApiResponse(HttpStatus status , CustomException e) {
+        this.status = status;
+        this.exception = e;
+    }
+
+    @Builder
+    public ApiResponse(Object data, HttpStatus status) {
+        this.data = data;
+        this.status = status;
+    }
+
+    @Builder
+    public ApiResponse(Object data, HttpStatus status, String statusText) {
+        this.data = data;
+        this.status = status;
+        this.statusText = statusText;
     }
 }
